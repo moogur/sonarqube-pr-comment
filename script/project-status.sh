b896d35891c762ef30cb1471b226706482be8a2d
Alexey <moogur-mail@yandex.ru>
2023-06-10 18:07:24 +0300

    [GA-271] feature(script): update

--- script/project-status.sh
+++ script/project-status.sh
@@ -1,32 +1,39 @@
 #!/usr/bin/env bash
 
-qualityGateUrl="${SONAR_HOST_URL}/api/qualitygates/project_status?projectKey=${SONAR_PROJECTKEY}"
-qualityGateStatus="$(curl --silent --fail --show-error --user "${SONAR_TOKEN}": "${qualityGateUrl}" | jq -r '.projectStatus.status')"
+projectInfoUrl="${SONAR_HOST_URL}/api/measures/component?component=${SONAR_PROJECT_KEY}&metricKeys=bugs,vulnerabilities,security_hotspots,security_hotspots_reviewed,code_smells,coverage,lines_to_cover,quality_gate_details,duplicated_lines_density,lines"
+project_info="$(curl --silent --fail --show-error --user "${SONAR_TOKEN}": "${projectInfoUrl}")"
 
-projectStatusUrl="${SONAR_HOST_URL}/api/qualitygates/project_status?projectKey=${SONAR_PROJECTKEY}"
-project_status="$(curl -s -u ${SONAR_TOKEN}: -G --data-urlencode --data-urlencode \
-${projectStatusUrl})"
+readarray -t arrayMetrics < <(jq -c '.component.measures[]' <<< "$project_info")
+result=
 
-codeOk=$(jq -r '.projectStatus.conditions[] | select(.status=="OK") | "\n✅Status: " + .status, "MetricKey: " + .metricKey, "Comparator: " + .comparator, "ErrorThreshold: " + .errorThreshold, "ActualValue: " + .actualValue' <<< "$project_status")
+for metricObj in "${arrayMetrics[@]}"; do
+  metric=$(jq -c '.metric' <<< "$metricObj")
+  value=$(jq -c '.value' <<< "$metricObj")
 
-codeFail=$(jq -r '.projectStatus.conditions[] | select(.status=="ERROR") | "\n💣Status: " + .status, "MetricKey: " + .metricKey, "Comparator: " + .comparator, "ErrorThreshold: " + .errorThreshold, "ActualValue: " + .actualValue' <<< "$project_status")
+  if [[ "$metric" == "\"quality_gate_details\"" ]]; then
+    prepared_value=$(echo "$value" | tr "\"" " " | awk '{ print $4 }') # value is 'OK' or 'ERROR'
 
-error="ERROR CONFIGURATION"
+    if [[ $prepared_value == "OK\\" ]]; then
+      prefix="✅"
+    else
+      prefix="💣"
+    fi
 
-codeValidation () {
+      result="$prefix Status: ${prepared_value%?}\n${result}"
+  else
+    prepared_metric=$(echo "${metric^}" | tr "_" " ")
+    result+="\n${prepared_metric:1:-1}: ${value:1:-1}"
+  fi
 
-if [[ ${qualityGateStatus} == "OK" ]]; then
-     echo "👋 Hey Quality Gate has PASSED.$codeOk"
-elif [[ ${qualityGateStatus} == "ERROR" ]]; then
-     echo "👋 Hey Quality Gate has FAILED.$codeFail"
-else
-   echo "quality_check=${error}" >> $GITHUB_OUTPUT
-fi
-}
-
-result=$(codeValidation)
+done
 
 echo "quality_check<<EOF" >> $GITHUB_OUTPUT
-echo "$result" >> $GITHUB_OUTPUT
-echo "EOF" >> $GITHUB_OUTPUT
 
+if [[ -z $result ]]; then
+  echo "UNKNOWN ERROR" >> "$GITHUB_OUTPUT"
+  echo "Message - $project_info" >> "$GITHUB_OUTPUT"
+else
+  echo "$result" >> "$GITHUB_OUTPUT"
+fi
+
+echo "EOF" >> "$GITHUB_OUTPUT"
