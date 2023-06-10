2a946ef8c8308cfd86d59d6695bab4e4ea11267c
Alexey <moogur-mail@yandex.ru>
2023-06-10 22:58:23 +0300

    [GA-271] feature(script): update

--- script/project-status.sh
+++ script/project-status.sh
@@ -25,7 +25,11 @@ for metricObj in "${arrayMetrics[@]}"; do
       title="$prefix Status: **${prepared_value%?}**"
   else
     prepared_metric=$(echo "${metric^}" | tr "_" " ")
-    table_body+="|${prepared_metric:1:-1}|${value:1:-1}|\n"
+    table_body+="|${prepared_metric:1:-1}|${value:1:-1}"
+    if [[ "$metric" == "\"security_hotspots_reviewed\"" ]] || [[ "$metric" == "\"duplicated_lines_density\"" ]] || [[ "$metric" == "\"coverage\"" ]]; then
+      table_body+="%"
+    fi
+    table_body+="|\n"
   fi
 
 done
